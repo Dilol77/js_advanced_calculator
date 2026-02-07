77b93109c063acc4549dbfcc8b1b7c742f225f0c
Dmytro Omelchuk <swat04123@gmail.com>
2026-02-07 13:15:13 +0200

    Implement makeCalculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,33 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calc = {
+    result: 0,
+    add(x) {
+      this.result = this.result + x;
+    },
+    subtract(x) {
+      this.result = this.result - x;
+    },
+    multiply(x) {
+      this.result = this.result * x;
+    },
+    divide(x) {
+      this.result = this.result / x;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(callback, number) {
+      callback.call(this, number);
+
+      return this;
+    },
+  };
+
+  return calc;
 }
 
 module.exports = makeCalculator;
